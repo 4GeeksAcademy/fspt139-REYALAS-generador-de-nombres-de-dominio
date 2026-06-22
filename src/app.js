c633315754fbede56bf68b19746746823f57b4a6
ReyAlas <jsalazargonzalez7@gmail.com>
2026-06-22 19:23:14 +0000

    trabajo acortado y funcional en pagina

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extenciones = [`.com`, `.net`, `.us`, `.io`];
+
+  pronoun.forEach(function (pronombreDeLaLista) {
+
+    adj.forEach(function (adjetivoDeLaLista) {
+
+      noun.forEach(function (sustanciaDeLaLista) {
+
+        let baseDomain = pronombreDeLaLista + adjetivoDeLaLista + sustanciaDeLaLista;
+        extenciones.forEach(function (extencionesDeLaLista) {
+          console.log(baseDomain + extencionesDeLaLista);
+        })
+
+      })
+
+    })
+
+  })
+
 };
